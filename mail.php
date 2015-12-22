58dbf37bb41c47f2fd1e99adf7c44f0feb7011b3
Natasha <natasha_born@mail.com>
2015-12-22 20:44:50 +0300

    22.12.44

--- mail.php
+++ mail.php
@@ -1,12 +1,38 @@
 <?php
-
-$recepient = "natasha_born@mail.ru";
-$sitename = "Название сайта";
-
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+if(isset($_POST)){
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 $full_text = trim($_POST["text"]);
 $message = "Имя: $name \nТелефон: $phone \nТекст: $full_text";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+require './PHPMailer/PHPMailerAutoload.php';
+$mail=new PHPMailer;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth=true;
+$mail->Username='natasha_born';
+$mail->Password='1590753born';
+$mail->SMTPSecure='TLS';
+$mail->Port='587';
+
+$mail->Charset='UTF-8';
+$mail->From= 'natasha_born@mail.ru';
+$mail->FromName='Natasha';
+$mail->addCC('natashaborn3371@gmail.com', 'Natasha');
+                  
+$mail->isHTML(true);
+                  
+$mail->Subject=$pagetitle;
+$mail->Body=$message;
+$mail->AltBody=$message;
+
+if($mail->send()){
+    echo 'pismo otpravleno';
+}else{
+    echo 'pismo ne mojet bit otpravleno';
+    echo 'error:'.$mail->ErrorInfo;
+}
+                  die;
+                  }
